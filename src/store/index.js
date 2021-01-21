c4ed22361f96f8f2ac04dea9f94c1406cb54562e
ekydpa21 <ekydodipratamaa21@gmail.com>
2021-01-21 20:48:14 +0700

    main game play

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,24 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    hp: 0,
+    score: 0
   },
   mutations: {
+    SOCKET_init (state, payload) {
+      state.hp = payload
+    },
+    SOCKET_currentHp (state, payload) {
+      state.hp = state.hp - payload
+    },
+    SOCKET_myScore (state, payload) {
+      state.score = state.score + payload
+    }
   },
   actions: {
-  },
-  modules: {
+    decreaseHp (context, payload) {
+      payload = 1
+      context.commit('hpMonster', payload)
+    }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 v-if="hpMonster > 0" >{{ hpMonster }}</h1>
+    <div v-if="hpMonster === 0" class="score">
+      <h2>Your Score</h2>
+      <h1>{{ score }}</h1>
+    </div>
+    <button v-if="hpMonster > 0" @click.prevent="attack">Attack</button>
+    <button v-if="hpMonster === 0">Play Again</button>
+    <button v-if="hpMonster === 0">Exit Room</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
+  data () {
+    return {
+      hp: 0
+    }
+  },
   components: {
-    HelloWorld
+  },
+  methods: {
+    attack () {
+      this.$socket.emit('attack', 1)
+      // this.$store.dispatch('decreaseHp')
+    }
+  },
+  computed: {
+    hpMonster () {
+      return this.$store.state.hp
+    },
+    score () {
+      return this.$store.state.score
+    }
+  },
+  sockets: {
   }
 }
 </script>
